97214c6ccad88c8512de893514b11cfa9349b8d2
Nathaniel Kuga <natelkuga@protonmail.com>
2024-02-14 18:43:17 -0800

    Created Ts test file for demo.

---
+++ KoboldPainting/KoboldPaintingJestTypeScriptUnitTestProject/tests/sum_wwwroot.test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum_wwwroot_1 = require("../../KoboldPainting/wwwroot/ts/sum_wwwroot");
+test('adds 1 + 2 to equal 3', () => {
+    expect((0, sum_wwwroot_1.sum)(1, 2)).toBe(3);
+});
+//# sourceMappingURL=sum_wwwroot.test.js.map
